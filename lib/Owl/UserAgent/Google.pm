6b67bb071803d0db536ee953ab49efc2a096d684
Simone Cesano <simonecesano@libero.it>
2017-04-10 14:48:50 +0200

    first commit in lean branch

--- lib/Owl/UserAgent/Google.pm
+++ lib/Owl/UserAgent/Google.pm
@@ -4,18 +4,13 @@ use Mojo::Base 'Mojo::UserAgent';
 
 has 'token';
 
-# check http://mojolicio.us/perldoc/Mojo/UserAgent#start
-
-# sub new {
-#     my $self = shift;
-
-#     return $self->SUPER::new;
-# }
-
 sub build_tx {
     my $self = shift;
     if ($self->token) {
 	my $tx = $self->SUPER::build_tx(@_);
+	print '-' x 120;
+	print STDERR $self->token->{access_token};
+	print '-' x 120;
 	$tx->req->headers->authorization('Bearer ' . $self->token->{access_token});
 	return $tx;
     } else {
